9cb9139c96b73939aa073362b51c01a3442fc9b2
Luke Benstead <kazade@gmail.com>
2025-02-18 14:52:47 +0000

    Fix recursion

--- djangae/contrib/googleauth/permissions.py
+++ djangae/contrib/googleauth/permissions.py
@@ -43,7 +43,7 @@ def get_permission_choices():
 
 
 class PermissionChoiceIterator(object):
-    def __iter__(self):
+    def __call__(self):
         for perm in get_permission_choices():
             yield perm
 
